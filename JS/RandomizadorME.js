f629ba9ed021e3e78f60198742b5d10e95801b73
Alonso Molero <alonsommc@gmail.com>
2023-10-01 19:31:23 -0500

    Se agregó arreglo de tipo de armas y la función para seleccionar las mismas

--- JS/RandomizadorME.js
+++ JS/RandomizadorME.js
@@ -3,10 +3,12 @@ const botonRandomizador = document.getElementById("boton-randomizar")
 const spanPersonaje = document.getElementById("personaje")
 const spanCarreraMilitar = document.getElementById("carrera-militar")
 const spanPoderExtra = document.getElementById("poder-extra")
+const spanTipoDeArma = document.getElementById("tipo-de-arma")
 
 let personajes = ["Jhon Shepard", "Jane Shepard"]
 let carrerasMilitares = ["Soldado", "Infiltrado", "Vanguardia", "Centinela", "Adepto", "Ingeniero"]
 let poderesExtra = ["Carnicería", "Tirador", "Mina de proximidad", "Dron de defensa", "Drenaje de energía", "Granada inferno", "Asaltar", "Estasis", "Munición de alteración", "Barrera", "Matriz defensiva", "Fortificación", "Munición perforante", "Estrellar", "Fuerza oscura"]
+let tiposDeArmas = ["Rifle Francotirador", "Rifle de Asalto", "Subfusil", "Escopeta"]
 
 function iniciarRandomizador() {
     botonRandomizador.addEventListener("click", seleccionarPersonaje)
@@ -32,6 +34,13 @@ function seleccionarPoderExtra() {
     spanPoderExtra.innerHTML = poderesExtra[poderExtra]
     seleccionarTipoDeArma()
 } 
+
+function seleccionarTipoDeArma() {
+    let tipoDeArma = aleatorio(0, tiposDeArmas.length -1)
+
+    spanTipoDeArma.innerHTML = tiposDeArmas[tipoDeArma]
+    seleccionarArmaPrincipal()
+}
 
 function aleatorio(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min)

cb29d03e1e637a5b3b698459074a0a1ad637efdb
Alonso Molero <alonsommc@gmail.com>
2023-10-01 21:49:21 -0500

    Se agregó estructuras para llenar el arreglo de armas principales segun el tipo de arma seleccionado, y la funcion para seleccionar el arma principal

--- JS/RandomizadorME.js
+++ JS/RandomizadorME.js
@@ -4,11 +4,19 @@ const spanPersonaje = document.getElementById("personaje")
 const spanCarreraMilitar = document.getElementById("carrera-militar")
 const spanPoderExtra = document.getElementById("poder-extra")
 const spanTipoDeArma = document.getElementById("tipo-de-arma")
+const spanArmaPrincipal = document.getElementById("arma-principal")
 
+let tipoDeArma
 let personajes = ["Jhon Shepard", "Jane Shepard"]
 let carrerasMilitares = ["Soldado", "Infiltrado", "Vanguardia", "Centinela", "Adepto", "Ingeniero"]
 let poderesExtra = ["Carnicería", "Tirador", "Mina de proximidad", "Dron de defensa", "Drenaje de energía", "Granada inferno", "Asaltar", "Estasis", "Munición de alteración", "Barrera", "Matriz defensiva", "Fortificación", "Munición perforante", "Estrellar", "Fuerza oscura"]
 let tiposDeArmas = ["Rifle Francotirador", "Rifle de Asalto", "Subfusil", "Escopeta"]
+let armasPrincipales = []
+let riflesFrancotiradores = ["Lanzaarpones Kishock", "Valiant de N7", "Black Widow", "Incisor M-29", "Indra M-90", "Jabalina", "Mantis M-92", "Raptor M-13", "Viper M-97", "Widow M-98", "Rifle Krysae"]
+let riflesDeAsalto = ["Argus M-55", "Avenger M-8", "Phaeston", "Fusil de Pulso Geth", "Mattock M-96", "Lanzachakrams", "Revenant M-76", "Valkyrie de N7", "Vindicator M-15", "Fusil de Particulas", "Harrier de Cerberus", "Fusil Antisintéticos Adas", "Fusil de Asalto Striker", "N7 Typhoon", "Geth Spitfire"]
+let subfusiles = ["Hornet M-25", "Locust M-12", "Shuriken M-4", "Tempest M-9", "Castig. Manada Sangrienta", "Hurricane de N7", "Subfusil de Plasma Geth"]
+let escopetas = ["Claymore M-300", "Discípulo", "Eviscerator M-22", "Escopeta de Plasma Geth", "Lanzapicos Graal", "Katana M-23", "Raider AT-12", "Scimitar M-27", "Wraith M-11", "Carabina Reegar", "Escopeta Venom", "N7 Piranha", "Crusader de N7"]
+let pistolasPesadas = ["Carnifex M-6", "Paladin M-77", "Phalanx M-5", "Predator M-3", "Scorpion", "Talon M-358", "Pistola de Arcos", "Pistola Executioner", "Eagle de N7"]
 
 function iniciarRandomizador() {
     botonRandomizador.addEventListener("click", seleccionarPersonaje)
@@ -36,10 +44,40 @@ function seleccionarPoderExtra() {
 } 
 
 function seleccionarTipoDeArma() {
-    let tipoDeArma = aleatorio(0, tiposDeArmas.length -1)
+    tipoDeArma = aleatorio(0, tiposDeArmas.length -1)
 
     spanTipoDeArma.innerHTML = tiposDeArmas[tipoDeArma]
     seleccionarArmaPrincipal()
+}
+
+function seleccionarArmaPrincipal() {
+    console.log(tipoDeArma)
+    if (tipoDeArma === 0) {
+        for (let index = 0; index < riflesFrancotiradores.length; index++) {
+            armasPrincipales.push(riflesFrancotiradores[index]);    
+        }
+    } 
+    else if (tipoDeArma === 1) {
+        for (let index = 0; index < riflesDeAsalto.length; index++) {
+            armasPrincipales.push(riflesDeAsalto[index]);    
+        }
+    }
+    else if (tipoDeArma === 2) {
+        for (let index = 0; index < subfusiles.length; index++) {
+            armasPrincipales.push(subfusiles[index]);    
+        }
+    } else {
+        for (let index = 0; index < escopetas.length; index++) {
+            armasPrincipales.push(escopetas[index]);    
+        }
+    }
+
+    console.log(armasPrincipales)
+    
+    let armaPrincipal = aleatorio(0, armasPrincipales.length -1)
+
+    spanArmaPrincipal.innerHTML = armasPrincipales[armaPrincipal]
+    seleccionarArmaSecundaria()
 }
 
 function aleatorio(min, max) {
